7b37bfd797658570f9018d4f5f7012f3b94a1d7e
Tzook <tzook10@gmail.com>
2022-12-02 11:02:48 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Stack } from '@mui/system';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Stack height="100%" width="100%">
+            hi
+        </Stack>
+    );
 }
 
 export default App;
